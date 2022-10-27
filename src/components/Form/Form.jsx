1a21531347eb8cd98ccebe03a7e54da10298cb5e
quinnkhuc <71853148+quinnkhuc@users.noreply.github.com>
2022-10-26 22:23:12 -0400

    Add form and cart functionalities

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,27 +1,50 @@
 import './Form.scss';
 import { useDispatch, useSelector } from 'react-redux';
-import { update } from '../../features/product';
+import { update_product } from '../../features/product';
+import { update_cart } from '../../features/cart';
+import PropTypes from 'prop-types';
 
 function Form(product){
     const products = useSelector((state) => state.product.value);
+    const cart = useSelector((state) => state.cart.value);
+
     const dispatch = useDispatch();
 
-    function handleInputChange(e){
-        let state = [...products];
-        state[e.target.id] = {
-            ...product,
-            [e.target.name]: e.target.value,
-        };
-        dispatch(update(state));
+    function handleInputChange(e, id){
+
+        //Update product
+        const index = products.findIndex(product => product.id === id);
+
+        const state = [...products];
+
+        state[index] = {
+            ...state[index],
+            [e.target.name]: e.target.value
+        }
+
+        dispatch(update_product(state))
+        
+        //Update cart
+        for(let i = 0; i < cart.length; i++){
+            console.log(cart[i].id)
+            if(id === cart[i].id){
+                const cart_state = [...cart];
+                cart_state[i] = {
+                    ...cart_state[i],
+                    [e.target.name]: e.target.value
+                }
+                dispatch(update_cart(cart_state))
+            }
+        }
     }
+
 
     return(
         <form>
             <div className='name'>
                 <input 
-                    id={product.id}
                     name='name'
-                    onChange={event => handleInputChange(event)}
+                    onChange={e => handleInputChange(e, product.id)}
                     placeholder='Product Name'
                     type='text'
                     value={product.name}
@@ -29,9 +52,8 @@ function Form(product){
             </div>
             <div className='price'>
                 <input 
-                    id={product.id}
                     name='price'
-                    onChange={event => handleInputChange(event)}
+                    onChange={e => handleInputChange(e, product.id)}
                     placeholder='9.99'
                     type='number'
                     value={product.price}
@@ -39,6 +61,10 @@ function Form(product){
             </div>
         </form>
     )
+}
+
+Form.propTypes = {
+    product: PropTypes.array
 }
 
 export default Form;
