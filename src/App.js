925b6a0ffa3f30e4ea3aaab4086824978a64df82
quinnkhuc <71853148+quinnkhuc@users.noreply.github.com>
2022-10-27 23:06:30 -0400

    Connect to firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
+
+import { collection, onSnapshot, query } from 'firebase/firestore'
+import { db } from './firestore'
+import { useDispatch } from 'react-redux';
+import { initialize_store } from './features/product';
 
 import Header from './components/Header/Header';
 import Catalog from './components/Catalog/Catalog';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const q = query(collection(db, 'products'))
+    onSnapshot(q, querySnapshot => {
+      setProducts([])
+      querySnapshot.forEach(doc => {
+        setProducts(prevProducts => [
+          ...prevProducts,
+          doc.data()
+        ])
+      })
+    })
+  }, [])
+
+  useEffect(() => {
+    dispatch(initialize_store(products))
+  })
+
 
   return (
     <>
