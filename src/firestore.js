c04d1fbd0c53f69f26e89075228161fc10ea9e51
quinnkhuc <71853148+quinnkhuc@users.noreply.github.com>
2022-11-15 18:48:13 -0500

    fix firestore.js

--- src/firestore.js
+++ src/firestore.js
@@ -4,7 +4,7 @@ import { getFirestore } from 'firebase/firestore'
 const firebaseConfig = {
   apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
   authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
-  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  projectId: 'idm363-25365',
   databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com/`,
 };
 
