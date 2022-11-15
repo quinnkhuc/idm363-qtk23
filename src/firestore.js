38b823d457fd7fb28f60163cd1736d030aebfd56
quinnkhuc <71853148+quinnkhuc@users.noreply.github.com>
2022-11-15 18:58:38 -0500

    update firebare

--- src/firestore.js
+++ src/firestore.js
@@ -4,7 +4,7 @@ import { getFirestore } from 'firebase/firestore'
 const firebaseConfig = {
   apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
   authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
-  projectId: 'idm363-25365',
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
   databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com/`,
 };
 
